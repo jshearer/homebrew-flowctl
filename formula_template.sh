09f9aa3fc8588b7f35d196d5f2789261fb36799d
Joseph Shearer <joseph@josephshearer.net>
2023-07-19 18:48:15 -0400

    fix

--- formula_template.sh
+++ formula_template.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 # e.g 0.1.3
-TAG_NAME=$FLOW_RELEASE_NAME
-ARCHIVE_URL="https://github.com/estuary/flow/archive/refs/tags/v{$TAG_NAME}.tar.gz"
+ARCHIVE_URL="https://github.com/estuary/flow/archive/refs/tags/v{$FLOW_RELEASE_NAME}.tar.gz"
 
 DOWNLOADS_BASE="https://github.com/estuary/flow/releases/download"
-MACOS_BINARY="{$DOWNLOADS_BASE}/v{$TAG_NAME}/flowctl-multiarch-macos"
-LINUX_X86_BINARY="{$DOWNLOADS_BASE}/v{$TAG_NAME}/flowctl-x86_64-linux"
+MACOS_BINARY="$DOWNLOADS_BASE/v$FLOW_RELEASE_NAME/flowctl-multiarch-macos"
+LINUX_X86_BINARY="$DOWNLOADS_BASE/v$FLOW_RELEASE_NAME/flowctl-x86_64-linux"
 
-ARCHIVE_SHA=$(curl -sL "$ARCHIVE_URL" | shasum -a 256)
-MACOS_SHA=$(curl -sL "$MACOS_BINARY" | shasum -a 256)
-LINUX_X86_SHA=$(curl -sL "$LINUX_X86_BINARY" | shasum -a 256)
+ARCHIVE_SHA=$(curl -sL "$ARCHIVE_URL" | shasum -a 256 | cut -f 1 -d " ")
+MACOS_SHA=$(curl -sL "$MACOS_BINARY" | shasum -a 256 | cut -f 1 -d " ")
+LINUX_X86_SHA=$(curl -sL "$LINUX_X86_BINARY" | shasum -a 256 | cut -f 1 -d " ")
 
 cat << EOF
 class Flowctl < Formula
   desc "Command line interface for Flow"
   homepage "https://github.com/estuary/flow"
-  url "https://github.com/estuary/flow/archive/refs/tags/v$TAG_NAME.tar.gz"
+  # When updating this formula to a new version, you need to update this url as well as the "sha256" and "version" below!
+  # For example: "shasum -a 256 v0.3.2.tar.gz"
+  url "https://github.com/estuary/flow/archive/refs/tags/v$FLOW_RELEASE_NAME.tar.gz"
   sha256 "$ARCHIVE_SHA"
   license "Business Source License 1.1"
-  version "$TAG_NAME"
+  version "$FLOW_RELEASE_NAME"
 
   on_macos do
     resource "flowctl-binary" do
